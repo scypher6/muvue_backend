dedc5eba541865017b0968889fb35f57f1a15ff7
scypher6 <scypher6@gmail.com>
2020-03-28 19:54:38 -0400

    post and delete routes for creating and deleting a review

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,12 @@ Rails.application.routes.draw do
   #     resources :likes
   # end
 
-  post '/users/:id/favorites', to: 'favorites#create'
   post '/users/:id/likes', to: 'likes#create'
   delete '/users/likes/:id', to: 'likes#destroy'
+  post '/users/:id/favorites', to: 'favorites#create'
   delete '/users/favorites/:id', to: 'favorites#destroy'
+  post 'users/:id/reviews/', to: 'reviews#create'
+  delete 'users/reviews/:id', to: 'reviews#destroy'
   get '/topmovies', to: 'movies#topmovies'
   get '/genres/action', to: 'genres#genre'
   get '/genres/comedy', to: 'genres#genre'
