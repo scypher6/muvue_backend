8228ebc01fcd0aa56008975fe60ef5e377c34d6b
scypher6 <scypher6@gmail.com>
2020-03-29 19:41:53 -0400

    generated review serializer

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,10 @@
 class ReviewsController < ApplicationController
+    before_action :authorized
+
     def create
-        
+        @movie = Movie.find(params[:movie_id])
+        @review = Review.create(user_id: params[:user_id], movie_id: @movie.id, content: params[:content])
+        render json: @movie
     end
 
     def destroy
